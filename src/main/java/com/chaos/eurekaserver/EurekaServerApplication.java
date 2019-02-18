3fb3ccc39e79284e5eac3e2fd3f6ed86e7a0414d
caojian <caojian@hualala.com>
2019-02-18 11:26:02 +0800

    1112

--- src/main/java/com/chaos/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/chaos/eurekaserver/EurekaServerApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -15,10 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication(scanBasePackages={"com.chaos"},exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @EnableEurekaServer
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
-//@ComponentScan(basePackages={"com.chaos"})
-//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-//@EnableCaching
-//@EnableEurekaClient
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
